ade7457b0b8048372d202af27889993f54065f7d
gadirlih <gadirlih@gmail.com>
2021-02-27 04:48:33 +0400

    add MRV and LCV

--- src/project2/BacktrackingSearch.java
+++ src/project2/BacktrackingSearch.java
@@ -8,85 +8,125 @@ public class BacktrackingSearch {
     private final HashMap<Integer, HashSet<Integer>> mDomainList;
     private final int mColorCount;
 
-    public BacktrackingSearch(Input input){
+    public BacktrackingSearch(Input input) {
         mAdjacencyList = input.getAdjacencyList();
         mDomainList = new HashMap<>();
         mColorCount = input.getColorCount();
         initDomain();
     }
 
-    private void initDomain(){
-        for(Integer var : mAdjacencyList.keySet()){
+    private void initDomain() {
+        for (Integer var : mAdjacencyList.keySet()) {
             HashSet<Integer> tempSet = new HashSet<>();
-            for(int i = 1; i <= mColorCount; i++){
+            for (int i = 1; i <= mColorCount; i++) {
                 tempSet.add(i);
             }
             mDomainList.put(var, tempSet);
         }
     }
 
-    public HashMap<Integer, Integer> startSearch(){
+    public HashMap<Integer, Integer> startSearch() {
         return search(new HashMap<>());
     }
 
     // assignment, Vertex <-> Color Number
-    private HashMap<Integer, Integer> search(HashMap<Integer, Integer> assignment){
-        if(assignment.keySet().size() == mAdjacencyList.keySet().size()){
+    private HashMap<Integer, Integer> search(HashMap<Integer, Integer> assignment) {
+        if (assignment.keySet().size() == mAdjacencyList.keySet().size()) {
             return assignment;
         }
 
         int variable = choseVariable(assignment);
 
-        ArrayList<Integer> variableDomain = new ArrayList<>(mDomainList.get(variable));
+        ArrayList<Integer> orderedDomain = orderDomain(variable);
 
-        for(int color : variableDomain){
+        for (int color : orderedDomain) {
             HashMap<Integer, Integer> copyAssignment = new HashMap<>(assignment);
             copyAssignment.put(variable, color);
 
-            AC3Result ac3Result = null;
-//            if(consistent(copyAssignment)){ // can delete it since ac3 handles it
-                ac3Result = AC3(variable, color);
-                if(ac3Result.isConsistent()){
-                    // update domain list from deletedValues
-                    for(Integer var : ac3Result.getDeletedValues().keySet()){
-                        for(Integer value : ac3Result.getDeletedValues().get(var)){
-                            mDomainList.get(var).remove(value);
-                        }
-                    }
-                }else continue;
-                HashMap<Integer, Integer> result = search(copyAssignment);
-                if(!result.isEmpty()){
-                    return result;
-                }
-//            }
-            // add back deletedValues to domainList
-//            if(ac3Result != null) {
+            AC3Result ac3Result = AC3(variable, color);
+            if (ac3Result.isConsistent()) {
+                // update domain list from deletedValues
                 for (Integer var : ac3Result.getDeletedValues().keySet()) {
                     for (Integer value : ac3Result.getDeletedValues().get(var)) {
-                        mDomainList.get(var).add(value);
+                        mDomainList.get(var).remove(value);
                     }
                 }
-//            }
+            } else continue;
+            HashMap<Integer, Integer> result = search(copyAssignment);
+            if (!result.isEmpty()) {
+                return result;
+            }
+            // add back deletedValues to domainList
+            for (Integer var : ac3Result.getDeletedValues().keySet()) {
+                for (Integer value : ac3Result.getDeletedValues().get(var)) {
+                    mDomainList.get(var).add(value);
+                }
+            }
         }
         return new HashMap<>();
     }
 
-    private int choseVariable(HashMap<Integer, Integer> assignment){
-        for (int var : mAdjacencyList.keySet()) {
-            if(!assignment.containsKey(var)){
-                return var;
+    // Apply MRV
+    // Chose variable which has minimum remaining values
+    // If there are more than variable with the same amount of minimum values
+    // Then chose based on the degree of these remaining variables
+    // Chose the one with the highest degree
+    private int choseVariable(HashMap<Integer, Integer> assignment) {
+        ArrayList<Integer> minimums = new ArrayList<>();
+        int minSize = Integer.MAX_VALUE;
+        for (Integer var : mDomainList.keySet()) {
+            if (!assignment.containsKey(var)) {
+                int domainSize = mDomainList.get(var).size();
+                if (domainSize < minSize) {
+                    minSize = domainSize;
+                    minimums.clear();
+                    minimums.add(var);
+                } else if (domainSize == minSize) {
+                    minimums.add(var);
+                }
             }
         }
-        return -1;
+        int maxDegreeVar = minimums.get(0);
+        if (minimums.size() > 1) {
+            int maxDegree = Integer.MIN_VALUE;
+            for (Integer var : minimums) {
+                int degree = mAdjacencyList.get(var).size();
+                if (degree > maxDegree) {
+                    maxDegree = degree;
+                    maxDegreeVar = var;
+                }
+            }
+            return maxDegreeVar;
+        }
+        return minimums.get(0);
     }
 
-    private boolean consistent(HashMap<Integer, Integer> assignment){
+    // Apply LCV
+    // Check domains of neighbours and sort the values based on how much they constrain neighbours
+    private ArrayList<Integer> orderDomain(int var) {
+        ArrayList<Integer> orderedDomain = new ArrayList<>();
+        ArrayList<Pair> orderedPairs = new ArrayList<>();
+        for (Integer val : mDomainList.get(var)) {
+            int count = 0;
+            for (Integer neighbour : mAdjacencyList.get(var)) {
+                if (mDomainList.get(neighbour).contains(val)) count++;
+            }
+            orderedPairs.add(new Pair(val, count));
+        }
+        orderedPairs.sort(Comparator.comparing(Pair::getSecond));
+        for (Pair pair : orderedPairs) {
+            orderedDomain.add(pair.getFirst());
+        }
+        return orderedDomain;
+    }
+
+    private boolean consistent(HashMap<Integer, Integer> assignment) {
         // check if all neighbours of each variable is consistent
-        for(Integer var : mAdjacencyList.keySet()){
+        for (Integer var : mAdjacencyList.keySet()) {
             ArrayList<Integer> neighbours = mAdjacencyList.get(var);
-            for(int neighbour : neighbours){
-                if(assignment.containsKey(var) && assignment.containsKey(neighbour)){
-                    if(assignment.get(var).equals(assignment.get(neighbour))){
+            for (int neighbour : neighbours) {
+                if (assignment.containsKey(var) && assignment.containsKey(neighbour)) {
+                    if (assignment.get(var).equals(assignment.get(neighbour))) {
                         return false;
                     }
                 }
@@ -95,7 +135,7 @@ public class BacktrackingSearch {
         return true;
     }
 
-    private AC3Result AC3(int var, int color){
+    private AC3Result AC3(int var, int color) {
         Queue<Pair> queue = new LinkedList<>();
         // deleted values from variables domain
         HashMap<Integer, ArrayList<Integer>> deletedValues = new HashMap<>();
@@ -103,8 +143,8 @@ public class BacktrackingSearch {
         // decrease domain of assigned variable to the only value chosen
         // 1. add deleted values to the deletedValues list
         ArrayList<Integer> valsDelteedFromDomain = new ArrayList<>();
-        for(Integer val : mDomainList.get(var)){
-            if(color != val){
+        for (Integer val : mDomainList.get(var)) {
+            if (color != val) {
                 // initialize list if absent and add value
                 deletedValues.computeIfAbsent(var, ArrayList::new);
                 deletedValues.get(var).add(val);
@@ -112,21 +152,21 @@ public class BacktrackingSearch {
             }
         }
         // 2. delete them from domainList
-        for(Integer val : valsDelteedFromDomain){
+        for (Integer val : valsDelteedFromDomain) {
             mDomainList.get(var).remove(val);
         }
 
-        for(Integer neighbour : mAdjacencyList.get(var)){
+        for (Integer neighbour : mAdjacencyList.get(var)) {
             queue.add(new Pair(neighbour, var));
         }
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Pair arc = queue.remove();
-            if(revise(arc.getFirst(), arc.getSecond(), deletedValues)){
-                if(mDomainList.get(arc.getFirst()).isEmpty()){
+            if (revise(arc.getFirst(), arc.getSecond(), deletedValues)) {
+                if (mDomainList.get(arc.getFirst()).isEmpty()) {
                     return new AC3Result(deletedValues, false);
                 }
                 // since First variable has changed we have to check its neighbours' consistency
-                for(Integer neighbour : mAdjacencyList.get(arc.getFirst())){
+                for (Integer neighbour : mAdjacencyList.get(arc.getFirst())) {
                     queue.add(new Pair(neighbour, arc.getFirst()));
                 }
             }
@@ -136,19 +176,18 @@ public class BacktrackingSearch {
 
     // Checks consistency between from and to variables
     // direction is from "from" variable to "to" variable
-    private boolean revise(int from, int to, HashMap<Integer, ArrayList<Integer>> deletedValues){
+    private boolean revise(int from, int to, HashMap<Integer, ArrayList<Integer>> deletedValues) {
         boolean revised = false;
         boolean consistent = false;
-        for(Iterator<Integer> i = mDomainList.get(from).iterator(); i.hasNext();){
+        for (Iterator<Integer> i = mDomainList.get(from).iterator(); i.hasNext(); ) {
             int xValue = i.next();
-            for(Iterator<Integer> j = mDomainList.get(to).iterator(); j.hasNext();){
-                int yValue = j.next();
+            for (int yValue : mDomainList.get(to)) {
                 if (xValue != yValue) {
                     consistent = true;
                     break;
                 }
             }
-            if(!consistent){
+            if (!consistent) {
                 //mDomainList.get(from).remove(xValue);
                 i.remove();
                 // initialize list if absent and add value
@@ -158,22 +197,6 @@ public class BacktrackingSearch {
             }
             consistent = false;
         }
-//        for(Integer xValue : mDomainList.get(from)){
-//            for(Integer yValue : mDomainList.get(to)){
-//                if (!xValue.equals(yValue)) {
-//                    consistent = true;
-//                    break;
-//                }
-//            }
-//            if(!consistent){
-//                mDomainList.get(from).remove(xValue);
-//                // initialize list if absent and add value
-//                deletedValues.computeIfAbsent(from, ArrayList::new);
-//                deletedValues.get(from).add(xValue);
-//                revised = true;
-//            }
-//            consistent = false;
-//        }
         return revised;
     }
 }
